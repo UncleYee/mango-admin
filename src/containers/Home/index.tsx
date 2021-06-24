004f43c1b5fdb3cd69ecce2189e3af0486af0d9d
uncleye <uncleye159@gmail.com>
2021-06-24 11:47:55 +0800

    feat: add eslint & antd

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import { Button } from 'antd';
+
 const Home = () => (
-  <div>Home</div>
+  <div>
+    Home
+    <Button>123111123</Button>
+  </div>
 );
 
 export default Home;
